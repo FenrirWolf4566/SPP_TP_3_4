e3a81f9c0c24b640d58158631b12e1a94676311b
FenrirWolf4566 <bdezordo@gmail.com>
2023-04-20 10:13:25 +0200

    Somes changes

--- src/GrayLevelFilter.java
+++ src/GrayLevelFilter.java
@@ -49,10 +49,10 @@ public class GrayLevelFilter extends FilterUtils {
     
 
     static public void main(String[] args) throws Exception {
-        ImageFilteringEngine engine = new ImageFilteringEngine();
+        SingleThreadedImageFilteringEngine engine = new SingleThreadedImageFilteringEngine();
         engine.loadImage("TEST_IMAGES/15226222451_5fd668d81a_c.jpg");
         engine.applyFilter(new GrayLevelFilter());
-        engine.writeOutPngImage("TEST_IMAGES/test_grayfilterfilter.png");
+        engine.writeOutPngImage("OUR_IMAGES/test_grayfilter.png");
       } // EndMain
     
 
