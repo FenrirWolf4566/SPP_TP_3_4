870180f26395068e7c3ca3c8e13c94944f5811a1
CodyAdam <cody.ad4@gmail.com>
2023-04-20 11:54:17 +0200

    feat: add tables

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # SPP_TP_3_4
+
+# Performance Analysis
+
+## Speed comparison per number of threads on the same image 
+
+- The image is 1920x1080 pixels. (**2073600 pixels**)
+- `k` is the number of threads.
+- The speed is the average of 10 runs.
+
+| Engine              | GrayLevelFilter | GaussianContourExtractorFilter |
+| ------------------- | --------------- | ------------------------------ |
+| Single Thread       | 130ms           | 21026ms                        |
+| Multi Thread (k=1)  | 106ms           | 20783ms                        |
+| Multi Thread (k=2)  | 76ms            | 10795ms                        |
+| Multi Thread (k=3)  | 58ms            | 8590ms                         |
+| Multi Thread (k=4)  | 45ms            | 7272ms                         |
+| Multi Thread (k=5)  | 40ms            | 6061ms                         |
+| Multi Thread (k=6)  | 39ms            | 6639ms                         |
+| Multi Thread (k=7)  | 52ms            | 5252ms                         |
+| Multi Thread (k=8)  | 35ms            | 6032ms                         |
+| Multi Thread (k=9)  | 39ms            | 5128ms                         |
+| Multi Thread (k=10) | 41ms            | 5717ms                         |
+
+## Speed comparison per image size on the same number of threads (k=4)
+
+- `n` is the number of pixels.
+- The speed is the average of 10 runs.
+
+| Image size                               | GrayLevelFilter | GaussianContourExtractorFilter |
+| ---------------------------------------- | --------------- | ------------------------------ |
+| 15226222451_75d515f540_o.jpg (n=2073600) | 43ms            | 4919ms                         |
+| 15226222451_a49b1a624b_h.jpg (n=1440000) | 23ms            | 3465ms                         |
+| 15226222451_5fd668d81a_c.jpg (n=360000)  | 6ms             | 889ms                          |
+| 15226222451_5fd668d81a_z.jpg (n=230400)  | 4ms             | 571ms                          |
+| 15226222451_5fd668d81a.jpg (n=140500)    | 9ms             | 348ms                          |
+| 15226222451_5fd668d81a_n.jpg (n=57280)   | 2ms             | 138ms                          |
+| 15226222451_5fd668d81a_m.jpg (n=32160)   | 1ms             | 88ms                           |
+| 15226222451_5fd668d81a_t.jpg (n=5600)    | 1ms             | 21ms                           |
--- src/PerformanceAnalysis.java
+++ src/PerformanceAnalysis.java
@@ -2,11 +2,15 @@ package src;
 
 public class PerformanceAnalysis {
   public static void main(String[] args) {
+    // part1();
+    part2();
+  }
+
+  public static void part1() {
     IFilter filterGray = new GrayLevelFilter();
     IFilter filterContour = new GaussianContourExtractorFilter();
 
     IImageFilteringEngine single = new SingleThreadedImageFilteringEngine();
-    IImageFilteringEngine multi4 = new MultiThreadedImageFilteringEngine(16);
 
     String img = "TEST_IMAGES/15226222451_75d515f540_o.jpg";
 
@@ -25,12 +29,50 @@ public class PerformanceAnalysis {
       System.out.print(perfOf(multi, img, filterGray) + " | ");
       System.out.println(perfOf(multi, img, filterContour) + " |");
     }
+  }
+
+  public static void part2() {
+    IFilter filterGray = new GrayLevelFilter();
+    IFilter filterContour = new GaussianContourExtractorFilter();
+
+    IImageFilteringEngine multi4 = new MultiThreadedImageFilteringEngine(16);
+
+    String[] images = new String[] {
+        "TEST_IMAGES/15226222451_75d515f540_o.jpg", // 1920 * 1080= 2073600
+        "TEST_IMAGES/15226222451_a49b1a624b_h.jpg", // 1600 * 900 = 1440000
+        "TEST_IMAGES/15226222451_5fd668d81a_c.jpg", // 800 * 450 = 360000
+        "TEST_IMAGES/15226222451_5fd668d81a_z.jpg", // 640 * 360 = 230400
+        "TEST_IMAGES/15226222451_5fd668d81a.jpg", // 500 * 281 = 140500
+        "TEST_IMAGES/15226222451_5fd668d81a_n.jpg", // 320 * 179 = 57280
+        "TEST_IMAGES/15226222451_5fd668d81a_m.jpg", // 240 * 134 = 32160
+        "TEST_IMAGES/15226222451_5fd668d81a_t.jpg", // 100 * 56 = 5600
+    };
+
+    int[] sizes = new int[] {
+        2073600,
+        1440000,
+        360000,
+        230400,
+        140500,
+        57280,
+        32160,
+        5600,
+    };
+
+    System.out.println("| Image size   | GrayLevelFilter | GaussianContourExtractorFilter |");
+    System.out.println("| ------------ | --------------- | ------------------------------ |");
+
+    for (int i = 0; i < images.length; i++) {
+      System.out.print("| "+ images[i].split("/")[1] +" (n=" + sizes[i] + ") | ");
+      System.out.print(perfOf(multi4, images[i], filterGray) + " | ");
+      System.out.println(perfOf(multi4, images[i], filterContour) + " |");
+    }
 
   }
 
   public static String perfOf(IImageFilteringEngine engine, String imgPath, IFilter filter) {
     long sum = 0;
-    int numberOfRuns = 1;
+    int numberOfRuns = 10;
     for (int i = 0; i < numberOfRuns; i++) {
       try {
         engine.loadImage(imgPath);
